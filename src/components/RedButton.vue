6d34bbbe976cc066b14ad5cedf72238515d48188
andriely oliveira <109562286+oliveira-andriely@users.noreply.github.com>
2023-06-04 14:30:34 -0300

    Primeiro componente utilizando vue

---
+++ src/components/RedButton.vue
@@ -0,0 +1,40 @@
+<!--HTML BRUTO-->
+<template>
+    <div>
+        <button>{{ text }}</button>
+    </div>
+</template>
+
+<!--Logica/ Typescript-->
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+    name: 'RedButton',
+    props: {
+        text: String,
+    },
+});
+
+</script>
+
+<!--Estilo-->
+<style>
+button{
+    background-color: rgb(146, 23, 23);
+    color: white;
+    border: 1px solid black;
+    width: 150px;
+    padding: 10px 10px;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 20px;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+button:hover{
+    background-color: rgb(229, 30, 30);
+}
+</style>
